e1d208af857b3d6cf14e37290aad2534776c3457
Alifaa00 <88605873+Alifaa00@users.noreply.github.com>
2024-11-30 23:36:36 +0700

    Update ForgotPassword.cy.js

--- cypress/POM/ForgotPassword.cy.js
+++ cypress/POM/ForgotPassword.cy.js
@@ -1,23 +1,29 @@
 class ForgotPasswordPage {
-    visit() {
-      cy.visit('https://opensource-demo.orangehrmlive.com/');
-    }
+  visit() {
+    cy.visit('https://opensource-demo.orangehrmlive.com/');
+  }
   
-    clickForgotPassword() {
-      cy.get('[class="orangehrm-login-forgot"]').click();
-    }
+  clickForgotPassword() {
+    cy.get('[class="orangehrm-login-forgot"]').click();
+  }
   
-    enterUsername(username) {
-        cy.get('[name="username"]').type(username);
-    }
+  enterUsername(username) {
+    cy.get('[name="username"]').type(username);
+  }
   
-    clickResetPassword() {
-      cy.get('[type="submit"]').click();
-    }
+  clickResetPassword() {
+    cy.get('[type="submit"]').click();
+  }
   
-    verifyResetMessage() {
-      cy.get('[class="orangehrm-card-container"]').should('be.visible');
-    }
+  verifyResetMessage() {
+    cy.get('[class="orangehrm-card-container"]').should('be.visible');
+  }
+
+  ErrorMessageRequired(){
+    cy.get('[class="oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message"]').should('have.text', 'Required');
+  }
+  
+
 }
 
 export default ForgotPasswordPage;  
