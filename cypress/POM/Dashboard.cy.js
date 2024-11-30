62102ca07f4a717391639abb3f7ee70e8f7d7fd2
Alifaa00 <88605873+Alifaa00@users.noreply.github.com>
2024-11-30 23:37:06 +0700

    Update Dashboard.cy.js

--- cypress/POM/Dashboard.cy.js
+++ cypress/POM/Dashboard.cy.js
@@ -1,26 +1,23 @@
 class DashboardPage {
-    visit() {
-      cy.visit('https://opensource-demo.orangehrmlive.com/');
-    }
+  visit() {
+    cy.visit('https://opensource-demo.orangehrmlive.com/');
+  }
   
-    loginAsAdmin(username, password) {
-        cy.get('[name="username"]').type(username);
-        cy.get('[name="password"]').type(password);
-        cy.get('[type="submit"]').click();
-    }
+  loginAsAdmin(username, password) {
+      cy.get('[name="username"]').type(username);
+      cy.get('[name="password"]').type(password);
+      cy.get('[type="submit"]').click();
+  }
   
-    navigateToDirectory() {
-      cy.get("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > aside:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(9) > a:nth-child(1) > span:nth-child(2)").click();
-    }
+  navigateToDirectory() {
+    cy.get("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > aside:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(9) > a:nth-child(1) > span:nth-child(2)").click();
+  }
     
-    verifyDirectoryPage() {
-      cy.url().should('include', '/directory');
-      cy.get('h6').should('contain.text', 'Directory');
-    }
+  verifyDirectoryPage() {
+    cy.url().should('include', '/directory');
+    cy.get('h6').should('contain.text', 'Directory');
+  }
 
-    SearchName(){
-        cy.get('[placeholder="Type for hints..."]');
-    }
 }
 
 
