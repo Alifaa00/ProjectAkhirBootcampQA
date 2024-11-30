d5d92ba28110f273676d134f0f684098476e9673
Alifaa00 <88605873+Alifaa00@users.noreply.github.com>
2024-11-30 23:33:23 +0700

    Update ForgotPassword.cy.js

--- cypress/e2e/ForgotPassword.cy.js
+++ cypress/e2e/ForgotPassword.cy.js
@@ -1,13 +1,24 @@
 import ForgotPasswordPage from '../POM/ForgotPassword.cy';
 
-describe('Forgot Password Tests', () => {
+describe('Forgot Password', () => {
   const forgotPasswordPage = new ForgotPasswordPage();
 
-  it('should send reset password request', () => {
+  it('Berhasil mengirimkan request reset password', () => {
     forgotPasswordPage.visit();
     forgotPasswordPage.clickForgotPassword();
     forgotPasswordPage.enterUsername('Admin');
+
+    cy.intercept('GET', '**/sendPasswordReset').as('btnReset');
     forgotPasswordPage.clickResetPassword();
+    cy.wait('@btnReset');
+
     forgotPasswordPage.verifyResetMessage();
+  });
+
+  it('Forgot dengan username kosong', () => {
+    forgotPasswordPage.visit();
+    forgotPasswordPage.clickForgotPassword();
+    forgotPasswordPage.clickResetPassword();
+    forgotPasswordPage.ErrorMessageRequired();
   });
 });
