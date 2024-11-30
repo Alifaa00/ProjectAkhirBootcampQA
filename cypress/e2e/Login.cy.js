ac9c5d20234d903e1e1b224ceaaed6e65e9a408d
Alifaa00 <88605873+Alifaa00@users.noreply.github.com>
2024-11-30 23:32:43 +0700

    Update Login.cy.js

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,21 +1,39 @@
+/// <reference types= "cypress" />
+
 import LoginPage from '../POM/Login.cy';
 
-describe('Login Tests', () => {
+describe('Login Test', () => {
   const loginPage = new LoginPage();
 
-  it('should login successfully with valid credentials', () => {
+  it('Berhasil login menggunakan data valid', () => {
     loginPage.visit();
     loginPage.fillUsername('Admin');
     loginPage.fillPassword('admin123');
+
+    cy.intercept('GET', '**/action-summary').as('login');
     loginPage.clickLoginButton();
+    cy.wait('@login');
+
     loginPage.LoginSuccess();
   });
 
-  it('should display error with invalid credentials', () => {
+  it('Gagal login menggunakan data tidak valid', () => {
     loginPage.visit();
     loginPage.fillUsername('User');
     loginPage.fillPassword('usertest');
+    
+    cy.intercept('GET', '**/login').as('btnlogin');
     loginPage.clickLoginButton();
-    loginPage.getErrorMessage();
+    cy.wait('@btnlogin');
+
+    loginPage.LoginInvalid();
   });
+
+  it('Gagal login dengan password kosong', () => {
+    loginPage.visit();
+    loginPage.fillUsername('Admin');
+    loginPage.clickLoginButton();
+
+    loginPage.LoginRequired();
+  })
 });
